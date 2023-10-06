14494aed20811cfc08f2e6f614a7d550b23874a7
pqcqaq <905739777@qq.com>
2023-10-06 16:46:49 +0800

    背景图

--- src/pages/posts/detail/cpns/UserInfo.vue
+++ src/pages/posts/detail/cpns/UserInfo.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="userInfo">
-    <image :src="props.user.background" mode="aspectFill" class="background" />
+  <div
+    class="userInfo"
+    :style="{
+      'background-image': 'url(' + props.user.background + ')',
+      'background-repeat': 'no-repeat',
+      width: '100%',
+      height: '100%',
+      'background-size': 'cover',
+      'background-position': 'center'
+    }"
+  >
     <view class="userInfo-user">
-      <image v-if="props.user.avatar != ''" :src="props.user.avatar" class="userInfo-user-avatar"></image>
+      <image v-if="props.user.avatar != ''" :src="props.user.avatar" class="userInfo-user-avatar" />
       <image v-else src="@/static/guest.png" class="userInfo-user-avatar" />
       <view class="userInfo-user-nickname">
         <view class="nickname">{{ props.user.nickName }}</view>
@@ -29,28 +38,16 @@ const props = defineProps({
 .userInfo-user:hover {
   background-color: rgb(220, 236, 255);
   border-radius: 16rpx;
+  opacity: 0.8;
 }
 .userInfo {
+  background-size: cover;
+  background-position: center;
   padding: 10rpx;
   position: relative;
   width: 100%;
   height: 100%;
 
-  .background {
-    // 作为整个view的背景
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    opacity: 0.4;
-    // 模糊
-    filter: blur(10);
-    // 穿透
-    pointer-events: none;
-    border-radius: 16rpx;
-  }
   width: 100%;
   background: #f6f9fe;
   border-radius: 16rpx;
