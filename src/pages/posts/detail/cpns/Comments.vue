c5e9a66f36213a6faef6018a5dd2f21036dd54c8
pqcqaq <905739777@qq.com>
2023-10-08 11:18:51 +0800

    评论区

--- src/pages/posts/detail/cpns/Comments.vue
+++ src/pages/posts/detail/cpns/Comments.vue
@@ -4,7 +4,24 @@
     <hd-toast></hd-toast>
     <hd-modal></hd-modal>
     <div class="header"></div>
-    <div class="body"></div>
+    <div class="body">
+      <!-- 左边显示用户头像（圆形） -->
+      <div class="left">
+        <div class="avatar">
+          <image v-if="comment.author?.avatar != ''" :src="comment.author?.avatar" class="userInfo-user-avatar" />
+          <image v-else src="@/static/guest.png" class="userInfo-user-avatar" />
+        </div>
+        <div>
+          <div class="nickname">{{ comment.author.nickName }}</div>
+          <div class="school">{{ comment.author.school }}</div>
+        </div>
+      </div>
+      <!-- 右边显示用户昵称、评论内容、评论时间 -->
+      <div class="right">
+        <div class="content">{{ comment.detail }}</div>
+        <div class="time">{{ comment.createTime }}</div>
+      </div>
+    </div>
     <div class="footer"></div>
   </div>
 </template>
@@ -23,11 +40,55 @@ const props = defineProps({
     required: true
   }
 })
+
+onMounted(() => {
+  console.log(props.comment)
+})
 </script>
 
 <style lang="scss" scoped>
 .body {
-  border: 10rpx solid #000;
-  height: 100rpx;
+  border: 2rpx solid #000;
+}
+.left {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+
+.nickname {
+  font-size: 30rpx;
+  color: #000;
+}
+.school {
+  font-size: 24rpx;
+  color: #000;
+}
+
+.content {
+  font-size: 28rpx;
+  color: #000;
+}
+
+.time {
+  font-size: 24rpx;
+  color: #00000075;
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+}
+.userInfo-user-avatar {
+  flex: 0 0 auto;
+  border-radius: 50%;
+  width: 90rpx;
+  height: 90rpx;
 }
 </style>
